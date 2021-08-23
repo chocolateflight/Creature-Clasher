08f96debd0881d4843f528899e143525296f6d43
Marc <67051612+chocolateflight@users.noreply.github.com>
2021-08-23 09:24:12 +0200

    Added Character Selection

--- PythonGame.py
+++ PythonGame.py
@@ -1,5 +1,7 @@
 # Hello World
 
+import random
+
 ###########################################################################
 
 # Logo Design #
@@ -35,12 +37,18 @@ class Creature:
         self.stamina = stamina
         self.attacks = attacks
         self.potions = potions
+    
+    def __repr__(self):
+        return self.name
 
 class Attacks:
     def __init__(self, name, health_effect, stamina_effect):
         self.name = name
         self.health_effect = health_effect
         self.stamina_effect = stamina_effect
+    
+    def __repr__(self):
+        return self.name
 
 class Potions(Attacks):
     pass
@@ -82,3 +90,60 @@ vampthing = Creature("Vampthing", 10, 150, 50, [vampire_bite, moonshock, invisib
 cryptbeast = Creature("Cryptbeast", 50, 80, 80, [electric_shock, hashfryer, market_crash], [healthpotion, healthpotion, staminapotion])
 glowtree = Creature("Glowtree", 120, 10, 90, [firethrow, chainsaw, falling_apple], [healthpotion, healthpotion, staminapotion])
 slagtooth = Creature("Slagtooth", 90, 90, 40, [strongbite, monstercrush, fingerflip], [staminapotion, staminapotion])
+
+creaturelist = [frostboy, vampthing, cryptbeast, glowtree, slagtooth]
+
+###########################################################################
+
+# Game #
+
+def print_pick_creature():
+    print("""
+Please pick a creature!
+
+1 - Frostboy
+2 - Vampthing
+3 - Cryptbeast
+4 - Glowtree
+5 - Slagtooth
+6 - Random
+
+""")
+
+def creature_selection(lst):
+    while True:
+
+        try:
+            character = int(input("Enter number: "))
+        except: 
+            print("\nPlease enter a valid number\n")
+            continue
+
+        if (character < 1) or ((len(lst) + 1) < character):
+            print("\nPlease enter a valid number\n")
+            continue
+        else:
+            break
+
+    if character == (len(lst) + 1):
+        character = lst[random.randint(0, (len(lst) - 1))]
+    else:
+        character = lst[character - 1]
+    
+    return character
+
+print_logo()
+print("Welcome to Creature Clash!")
+
+while True:
+
+    print_pick_creature()
+
+    player = creature_selection(creaturelist)
+
+    print("\nYou choose " + str(player) + " ! Now it's time to pick your opponent.\n")
+    print_pick_creature()
+
+    opponent = creature_selection(creaturelist)
+    print("\nYour opponent is " + str(opponent) + ". Good luck!")
+
