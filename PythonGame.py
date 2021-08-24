4bd92088dbb00e44e1bd7f46684bd40111ef6937
Marc <67051612+chocolateflight@users.noreply.github.com>
2021-08-24 17:51:43 +0200

    Removed functions and added code

--- PythonGame.py
+++ PythonGame.py
@@ -9,29 +9,106 @@ print("Welcome to Creature Clash!")
 
 while True:
 
-    python_game_functions.print_pick_creature()
+    python_game_functions.print_pick_creature() # Prints List of creatures
 
-    player = python_game_functions.creature_selection(creaturelist, python_game_functions.user_input)
+    player = python_game_functions.creature_selection(creaturelist, python_game_functions.user_input) # Allows user to pick a creature for themselves
 
     python_game_functions.print_line()
     print("\nYou choose " + str(player) + "! Now it's time to pick your opponent.\n")
     python_game_functions.print_pick_creature()
 
-    opponent = python_game_functions.creature_selection(creaturelist, python_game_functions.user_input)
+    opponent = python_game_functions.creature_selection(creaturelist, python_game_functions.user_input) # Allows user to pick an opponent creature
     print("\nYour opponent is " + str(opponent) + ". Good luck!\n")
 
-    while True:
+
+    lostcode = 0
+    wincode = 0
+    while lostcode != 808 and wincode != 101:
 
         python_game_functions.print_line()
-        print("\nLet's go! It's your turn!\n")
+        print("\nIt's your turn!\n")
         print(player.summary_player() + "\n")
         python_game_functions.print_round_choices()
-        round_selection = python_game_functions.round_selection(python_game_functions.optionslist, player,python_game_functions.user_input, potionslist)
-        move_selection = python_game_functions.move_selection(round_selection, player, opponent, python_game_functions.user_input, python_game_functions.attack_move)
+
+        available_attacks = []
+
+        while lostcode != 808 and wincode != 101:
+            round_option = python_game_functions.user_input(python_game_functions.optionslist)
+
+            if round_option == 1:
+
+                for attack in player.attacks:
+                    if abs(attack.stamina_effect) <= player.stamina:
+                        available_attacks.append(attack)
+                
+                if available_attacks != []:
+                    print("Choose an option:\n\n")
+                    i = 0
+                    for attack in available_attacks:
+                        print(str(i + 1) + " - " + str(attack))
+                        i += 1
+                    print("\n")
+                
+                    attack_number = python_game_functions.user_input(available_attacks)
+
+                    if attack_number == 1:
+                        output = python_game_functions.attack_move(player, opponent, available_attacks, attack_number)
+                        player.stamina = output[0]
+                        opponent.health = output[1]
+                        opponent.armour = output[2]
+                        break
+                    elif attack_number == 2:
+                        output = python_game_functions.attack_move(player, opponent, available_attacks, attack_number)
+                        player.stamina = output[0]
+                        opponent.health = output[1]
+                        opponent.armour = output[2]
+                        break
+                    elif attack_number == 3:
+                        output = python_game_functions.attack_move(player, opponent, available_attacks, attack_number)
+                        player.stamina = output[0]
+                        opponent.health = output[1]
+                        opponent.armour = output[2]
+                        break
+                elif (available_attacks == []) and (potionslist[0] in player.potions):
+                    print("You need to drink a stamina potion if you want to attack your opponent.")
+                    continue
+                elif (available_attacks == []) and not (potionslist[0] in player.potions):
+                    lostcode = 808
+                    break
+            
+            if round_option == 2:
+                if player.potions == []:
+                    print("\nNo more potions left. Pick another option.\n")
+                    continue
+                else:
+                    print("Choose an option:\n\n")
+
+                    i = 0
+                    for potion in player.potions:
+                        print(str(i + 1) + " - " + str(potion))
+                        i += 1
+                    print("\n")
+
+                    potion = python_game_functions.user_input(player.potions)
+
+                    if potion == 1:
+                        print("Potion 1")
+                        break # Add code for potion 1
+                    if potion == 2:
+                        print("Potion 2")
+                        break # Add code for potion 2
+                    if potion == 3:
+                        print("Potion 3")
+                        break # Add code for potion 3
+            
+            if round_option == 3:
+                print("Skip turn")
+                break # Add code for skip turn
+            
         break
     break
 
 python_game_functions.print_line()
-print(player)
-print(round_selection)
-print(move_selection)
+print(vars(player))
+print(vars(opponent))
+python_game_functions.print_line()
