901691222fa8ceae377207d7ad7e451c54b642c2
Marc <67051612+chocolateflight@users.noreply.github.com>
2021-08-24 11:30:06 +0200

    Changed code to match updated functions

--- PythonGame.py
+++ PythonGame.py
@@ -11,13 +11,13 @@ while True:
 
     python_game_functions.print_pick_creature()
 
-    player = python_game_functions.creature_selection(creaturelist)
+    player = python_game_functions.creature_selection(creaturelist, python_game_functions.user_input)
 
     python_game_functions.print_line()
     print("\nYou choose " + str(player) + "! Now it's time to pick your opponent.\n")
     python_game_functions.print_pick_creature()
 
-    opponent = python_game_functions.creature_selection(creaturelist)
+    opponent = python_game_functions.creature_selection(creaturelist, python_game_functions.user_input)
     print("\nYour opponent is " + str(opponent) + ". Good luck!\n")
 
     while True:
@@ -26,11 +26,12 @@ while True:
         print("\nLet's go! It's your turn!\n")
         print(player.summary_player() + "\n")
         python_game_functions.print_round_choices()
-        round_selection = python_game_functions.round_selection(python_game_functions.optionslist, player)
-        move_selection = python_game_functions.move_selection(round_selection, player)
+        round_selection = python_game_functions.round_selection(python_game_functions.optionslist, player,python_game_functions.user_input)
+        move_selection = python_game_functions.move_selection(round_selection, player, python_game_functions.user_input)
         break
     break
 
 python_game_functions.print_line()
 print(player)
 print(round_selection)
+print(move_selection)
